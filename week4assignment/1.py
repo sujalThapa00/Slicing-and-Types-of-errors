abfe629e94089b67607d63b7e5359090f15e5aa0
sujalThapa00 <thapasujal2063@gmail.com>
2024-12-27 20:10:07 +0545

    Add files via upload

---
+++ week4assignment/1.py
@@ -0,0 +1,28 @@
+# for i in range(10):
+#     print("softwarica")
+
+# a = [1,2,3,4,5]
+# sum = 0 
+# for i in range(1,6):
+#     sum +=i
+# print(f"sum is {sum}")
+
+# a="hello"
+# for i in range(len(a)):
+#     print(a[i],"=",i)
+
+# given_list = [1, "a", "c", 2, 3, 4]
+# for element in given_list:
+#     if isinstance(element, int):
+#         print(element)
+        
+# list = [4,5,3,2]
+# m = 1
+# for i in list:
+#     m = m*i
+# print(f"the multiplication is {m}")
+
+a =int(input("enter a number: "))
+for i in range(1,11):
+    m = a*i
+    print(f"{a} x {i}={m}")
